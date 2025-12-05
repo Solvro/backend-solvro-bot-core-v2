d23c0ebbb8f38c2da3f224688cf5d71c37832d5a
loudsheep <loudsheep05@gmail.com>
2025-12-05 14:12:32 +0100

    feeat: add basic necord configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import Joi, { allow } from 'joi';
+import { NecordModule } from 'necord';
+import { IntentsBitField } from 'discord.js';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-    envFilePath: '.env',
-    validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('development', 'production', 'test').default('development'),
-        DATABASE_URL: Joi.string().uri().required(),
-        PORT: Joi.number().default(3000),
+    imports: [ConfigModule.forRoot({
+        isGlobal: true,
+        envFilePath: '.env',
+        validationSchema: Joi.object({
+            NODE_ENV: Joi.string().valid('development', 'production', 'test').default('development'),
+            DATABASE_URL: Joi.string().uri().required(),
+            PORT: Joi.number().default(3000),
+            DISCORD_TOKEN: Joi.string().required(),
+            DISCORD_DEVELOPMENT_GUILD_ID: Joi.string().allow('', null),
+        }),
     }),
-  })],
-  controllers: [AppController],
-  providers: [AppService],
+    NecordModule.forRootAsync({
+        useFactory: (configService) => ({
+            token: configService.get('DISCORD_TOKEN'),
+            intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.GuildMembers],
+            development: [configService.get('DISCORD_DEVELOPMENT_GUILD_ID')]
+        }),
+        inject: [ConfigService],
+    })
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import * as necord from 'necord';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    getHello(): string {
+        return 'Hello World!';
+    }
+
+    @necord.SlashCommand({
+        name: 'ping',
+        description: 'Replies with Pong!',
+    })
+    public async onPing(@necord.Context() [interaction]: necord.SlashCommandContext) {
+        return interaction.reply({ content: 'Pong!' });
+    }
 }
