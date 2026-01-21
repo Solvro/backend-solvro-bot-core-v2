7c59c99b6c8b910f840057f690f4c4b24031a0eb
loudsheep <loudsheep05@gmail.com>
2026-01-21 08:08:10 +0100

    feat: add summary field to Meeting model and implement related file generation methods

--- src/core/recordings/recordings.service.ts
+++ src/core/recordings/recordings.service.ts
@@ -6,35 +6,45 @@ import { UpdateMeetingDTO } from './dto/update-meeting.dto';
 
 @Injectable()
 export class RecordingsService {
-    constructor(private database: DatabaseService) {}
-    
-    async updateMeeting(id: number, updateMeetingDTO: UpdateMeetingDTO) {
-        const existingMeeting = await this.database.meeting.findUnique({ where: { id: +id } });
+  constructor(private database: DatabaseService) { }
 
-        if (!existingMeeting) throw new NotFoundException('Meeting not found');
+  async updateMeeting(id: number, updateMeetingDTO: UpdateMeetingDTO) {
+    const existingMeeting = await this.database.meeting.findUnique({ where: { id: +id } });
 
-        return this.database.meeting.update({
-            where: { id: id },
-            data: {
-                fullTranscription: updateMeetingDTO.text,
-                recordingStatus: RecordingState.Processed,
-                finishedAt: new Date(),
-                transcriptionChunks: {
-                    create: [
-                        ...updateMeetingDTO.segments.map(chunk => ({
-                            speakerDiscordId: chunk.userId,
-                            startTime: chunk.start,
-                            duration: chunk.end - chunk.start,
-                            text: chunk.text
-                        }))
-                    ]
-                }
-            }
-        });
-    }
+    if (!existingMeeting) throw new NotFoundException('Meeting not found');
 
-    async saveSummary(id: number, meetingSummaryDTO: MeetingSummaryDTO) {
-        // TODO: implement GoogleDriveService to save the summary there
-        throw new NotImplementedException('Not implemented yet');
-    }
+    return this.database.meeting.update({
+      where: { id: id },
+      data: {
+        fullTranscription: updateMeetingDTO.text,
+        recordingStatus: RecordingState.Processed,
+        finishedAt: new Date(),
+        transcriptionChunks: {
+          create: [
+            ...updateMeetingDTO.segments.map(chunk => ({
+              speakerDiscordId: chunk.userId,
+              startTime: chunk.start,
+              duration: chunk.end - chunk.start,
+              text: chunk.text
+            }))
+          ]
+        }
+      }
+    });
+  }
+
+  async saveSummary(meetingId: number, meetingSummaryDTO: MeetingSummaryDTO) {
+    const existingMeeting = await this.database.meeting.findUnique({ where: { id: +meetingId } });
+
+    if (!existingMeeting) throw new NotFoundException('Meeting not found');
+
+    // TODO: implement GoogleDriveService to save the summary there
+
+    return this.database.meeting.update({
+      where: { id: meetingId },
+      data: {
+        summary: meetingSummaryDTO.summary,
+      }
+    });
+  }
 }
